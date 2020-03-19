f125278d103c9163bcd5e295d88b495eba955aa6
Zoltan Farkas <f.zoli@mailbox.hu>
2020-03-19 08:39:28 +0100

    primitive type tests

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -2,12 +2,16 @@ package sample;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(Parameterized.class)
 public class SampleTest {
@@ -36,11 +40,12 @@ public class SampleTest {
         if (withFactory) {
             // NOTE:
             // OptionalTypeTypeAdapterFactory must be registered before PhoneNumberTypeAdapterFactory.
-            // Prefer to use PhoneNumberTypeAdapter.
             builder.registerTypeAdapterFactory(new PhoneNumberTypeAdapterFactory(phoneNumberParser));
         }
         builder.registerTypeAdapterFactory(new ValidatingTypeAdapterFactory());
         if (!withFactory) {
+            // NOTE:
+            // It fails in case of primitive types (standalone or in collection).
             builder.registerTypeAdapter(PhoneNumber.class, new PhoneNumberTypeAdapter(phoneNumberParser));
         }
         return builder;
@@ -103,6 +108,81 @@ public class SampleTest {
                 .phoneNumber(PhoneNumber.absent())
                 .build();
         SampleData request = gson.fromJson(requestJson, SampleData.class);
+        Assert.assertEquals(expectedRequest, request);
+    }
+
+    @Test
+    public void serializeArrayWithPhoneNumbers() {
+        Gson gson = gsonBuilder().create();
+        String expectedJson = "[\"+36201234567\",\"+36301234567\"]";
+        List<PhoneNumber> request = Arrays.asList(PhoneNumber.raw("+36201234567"), PhoneNumber.raw("+36301234567"));
+        String json = gson.toJson(request);
+        Assert.assertEquals(expectedJson, json);
+    }
+
+    @Test
+    public void parseArrayWithPhoneNumbers() {
+        Gson gson = gsonBuilder().create();
+        String requestJson = "[\"+36201234567\",\"+36301234567\"]";
+        List<PhoneNumber> expectedRequest = Arrays.asList(PhoneNumber.raw("+36201234567"), PhoneNumber.raw("+36301234567"));
+        Type listType = new TypeToken<List<PhoneNumber>>() {}.getType();
+        List<PhoneNumber> request = gson.fromJson(requestJson, listType);
+        Assert.assertEquals(expectedRequest, request);
+    }
+
+    @Test
+    public void serializeArrayWithAbsentPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String expectedJson = "[null,\"+36301234567\"]";
+        List<PhoneNumber> request = Arrays.asList(PhoneNumber.absent(), PhoneNumber.raw("+36301234567"));
+        String json = gson.toJson(request);
+        Assert.assertEquals(expectedJson, json);
+    }
+
+    @Test
+    public void parseArrayWithNullPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String requestJson = "[null,\"+36301234567\"]";
+        List<PhoneNumber> expectedRequest = Arrays.asList(PhoneNumber.absent(), PhoneNumber.raw("+36301234567"));
+        Type listType = new TypeToken<List<PhoneNumber>>() {}.getType();
+        List<PhoneNumber> request = gson.fromJson(requestJson, listType);
+        Assert.assertEquals(expectedRequest, request);
+    }
+
+    @Test
+    public void parseArrayWithEmptyPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String requestJson = "[\"\",\"+36301234567\"]";
+        List<PhoneNumber> expectedRequest = Arrays.asList(PhoneNumber.absent(), PhoneNumber.raw("+36301234567"));
+        Type listType = new TypeToken<List<PhoneNumber>>() {}.getType();
+        List<PhoneNumber> request = gson.fromJson(requestJson, listType);
+        Assert.assertEquals(expectedRequest, request);
+    }
+
+    @Test
+    public void serializeAbsentPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String expectedJson = "null";
+        PhoneNumber request = PhoneNumber.absent();
+        String json = gson.toJson(request);
+        Assert.assertEquals(expectedJson, json);
+    }
+
+    @Test
+    public void parseEmptyPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String requestJson = "\"\"";
+        PhoneNumber expectedRequest = PhoneNumber.absent();
+        PhoneNumber request = gson.fromJson(requestJson, PhoneNumber.class);
+        Assert.assertEquals(expectedRequest, request);
+    }
+
+    @Test
+    public void parseNullPhoneNumber() {
+        Gson gson = gsonBuilder().create();
+        String requestJson = "null";
+        PhoneNumber expectedRequest = PhoneNumber.absent();
+        PhoneNumber request = gson.fromJson(requestJson, PhoneNumber.class);
         Assert.assertEquals(expectedRequest, request);
     }
 
